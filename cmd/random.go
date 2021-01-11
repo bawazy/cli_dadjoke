6b28ba25f88df9792a58650e7d1f32edaf3cfd88
bawazy <hbawazy@gmail.com>
2021-01-10 18:07:28 -0800

    first commit

---
+++ cmd/random.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Get a random dad joke",
+	Long:  `This command fetches a random dad joke from the icanhazdadjoke api`,
+	Run: func(cmd *cobra.Command, args []string) {
+		getRandomJoke()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+}
+
+type Joke struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
+}
+
+func getRandomJoke() {
+	url := "https://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
+
+	if err := json.Unmarshal(responseBytes, &joke); err != nil {
+		log.Printf("Could not Unmarshal response - %v", err)
+	}
+	fmt.Println(string(joke.Joke))
+}
+
+func getJokeData(baseApi string) []byte {
+	request, err := http.NewRequest(http.MethodGet, baseApi, nil)
+	if err != nil {
+		log.Printf("Could not Request a dadjoke - %v", err)
+	}
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "DadJoke CLI (github.com/bawazy/cli_dadjoke)")
+
+	response, err := http.DefaultClient.Do((request))
+	if err != nil {
+		log.Printf("Could not make a request - %v ", err)
+	}
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Could not read response body - %v", err)
+	}
+
+	return responseBytes
+}
